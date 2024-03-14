f9b6f6c71de454f4f1b8f8be83c832dffc646c49
venugopal <bvenugopalreddy16@gmail.com>
2024-03-14 21:15:14 +0530

    serializers and requirements.txt

---
+++ tags/serializer.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from tags.models import Tags 
+
+
+class WriteTagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tags
+        fields = ['name', 'slug']
+
+class ReadTagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tags
+        fields = ['name', 'slug', 'created_at', 'updated_at']
